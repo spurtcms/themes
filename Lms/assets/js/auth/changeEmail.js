8d576f636d54783b1614e22c93754d0aaf37d9b5
spurtcmsdeveloper-146 <119719731+parvesh-piccosoft@users.noreply.github.com>
2024-02-14 12:16:25 +0530

    new changes

--- Lms/assets/js/auth/changeEmail.js
+++ Lms/assets/js/auth/changeEmail.js
@@ -231,3 +231,71 @@ $(document).on("click", "#againotp", function () {
 
 
 })
+/* Resend Otp */
+$(document).on("click", "#againotp1", function () {
+
+    var url = window.location.search;
+    const urlpar = new URLSearchParams(url);
+    email = urlpar.get('emailid');
+
+
+    $.ajax({
+        url: "/send-otp-genrate1",
+        method: "POST",
+        data: { "email": email },
+        datatype: 'json',
+        success: function (data) {
+
+            if (data.verify == true) {
+
+                $.toast({
+                    text: "otp sended successfully", // Text that is to be shown in the toast
+                    heading: 'Note', // Optional heading to be shown on the toast
+                    icon: 'success', // Type of toast icon
+                    showHideTransition: 'fade', // fade, slide or plain
+                    allowToastClose: true, // Boolean value true or false
+                    hideAfter: 3000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+                    stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+                    position: 'top-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+
+
+
+                    textAlign: 'left',  // Text alignment i.e. left, right or center
+                    loader: true,  // Whether to show loader or not. True by default
+                    loaderBg: '#9EC600',  // Background color of the toast loader
+                    beforeShow: function () { }, // will be triggered before the toast is shown
+                    afterShown: function () { }, // will be triggered after the toat has been shown
+                    beforeHide: function () { }, // will be triggered before the toast gets hidden
+                    afterHidden: function () { }  // will be triggered after the toast has been hidden
+                });
+            } else {
+
+                $.toast({
+                    text: data.verify, // Text that is to be shown in the toast
+                    heading: 'Note', // Optional heading to be shown on the toast
+                    icon: 'warning', // Type of toast icon
+                    showHideTransition: 'fade', // fade, slide or plain
+                    allowToastClose: true, // Boolean value true or false
+                    hideAfter: 3000, // false to make it sticky or number representing the miliseconds as time after which toast needs to be hidden
+                    stack: 5, // false if there should be only one toast at a time or a number representing the maximum number of toasts to be shown at a time
+                    position: 'top-right', // bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values
+
+
+
+                    textAlign: 'left',  // Text alignment i.e. left, right or center
+                    loader: true,  // Whether to show loader or not. True by default
+                    loaderBg: '#9EC600',  // Background color of the toast loader
+                    beforeShow: function () { }, // will be triggered before the toast is shown
+                    afterShown: function () { }, // will be triggered after the toat has been shown
+                    beforeHide: function () { }, // will be triggered before the toast gets hidden
+                    afterHidden: function () { }  // will be triggered after the toast has been hidden
+                });
+            }
+
+
+
+        }
+    })
+
+
+})
